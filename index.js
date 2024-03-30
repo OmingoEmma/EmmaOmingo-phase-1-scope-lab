2100ef46ec4aee3f8b74ccd95ddd026f479df762
OmingoEmma <emmaomingo@gmail.com>
2024-03-31 02:37:20 +0300

    Complete Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+'use strict';
+
+// Declare customerName to be bob in global scope
+const customerName = 'bob';
+Object.freeze(customerName);
+
+// Function to modify the customerName variable
+function upperCaseCustomerName() {
+  return window.customerName = customerName.toUpperCase();
+
+}
+
+
+// Function to set bestCustomer
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+// Function to overwrite best customer
+function overwriteBestCustomer(newBestCustomer) {
+  window.bestCustomer = newBestCustomer;
+}
+
+// Function to simulate an error when trying to reassign the least favorite customer
+function changeLeastFavoriteCustomer() {
+  throw new Error('Assignment to constant variable.');
+}
+
+// Export functions for testing
+module.exports = {
+    upperCaseCustomerName,
+    setBestCustomer,
+    overwriteBestCustomer,
+    changeLeastFavoriteCustomer
+};
